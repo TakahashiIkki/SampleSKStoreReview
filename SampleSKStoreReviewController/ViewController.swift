281122dc02682937074a482972a5603ff954d13b
TakahashiIkki <new_ittyandesu@outlook.jp>
2018-06-14 23:59:37 +0900

    First Commit

---
+++ SampleSKStoreReviewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleSKStoreReviewController
+//
+//  Created by 一騎高橋 on 2018/06/14.
+//  Copyright © 2018年 一騎高橋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
