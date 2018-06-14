e8fba0c9995bc250267ba412b002baedbb0b61ad
TakahashiIkki <new_ittyandesu@outlook.jp>
2018-06-15 00:06:42 +0900

    取り敢えず雑に追加してみた。

--- SampleSKStoreReviewController/ViewController.swift
+++ SampleSKStoreReviewController/ViewController.swift
@@ -7,19 +7,23 @@
 //
 
 import UIKit
+import StoreKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if #available(iOS 10.3, *) {
+            SKStoreReviewController.requestReview()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
